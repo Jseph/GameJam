45cd83d0f47bf4ab388cc0b6d14a086bcf898773
Daniel <Daniel@Daniel-PC>
2014-11-07 23:22:12 -0600

    newmessage around11pm

--- src/Blob.java
+++ src/Blob.java
@@ -3,9 +3,8 @@ import java.awt.geom.Point2D;
 
 public class Blob
 {
-	public double orientation;
-	public double width;
-	public double height;
+	public double orientation;//0 is vertical (on top of a horizontal plane)
+	public double aspectratio;
 	public double unstressedsize;
 	public Point2D center;
 	public double VelocityX;
@@ -13,11 +12,14 @@ public class Blob
 	public Blob(double size, Point2D location)
 	{
 		orientation = 2*Math.PI;
-		width = size;
-		height = size;
 		unstressedsize = size;
 		center = location;
+		aspectratio = 1;
 		VelocityX = 0;
 		VelocityY = 0;
+	}
+	public double TangentalVelocity()
+	{
+		return VelocityX*Math.cos
 	}
 }
--- src/PhysicsEngine.java
+++ src/PhysicsEngine.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class PhysicsEngine
 {
-	private final int maxspeed = 20;
-	private final int gravity = 10;
-	private final int directionalinfluince = 10;
-	private final int dampening = 3;
-	private final int restoringforce = 10;
-	
+	private final double gravity = 10;
+	private final double directionalinfluince = 10;
+	private final double dampening = 3;
+	private final double restoringforce = 10;
+	private final double MaxAspectRatio = 10;
+	private final double tension = 2;
 	
 	public Level level;
 	public Blob blob;
@@ -24,21 +24,52 @@ public class PhysicsEngine
 		double NetForceRight = 0;
 		if(colliding())
 		{
+			Surface s = SurfaceColiding();
+			double NetForceTangant = 0;
+			double NetForceNormal = 0;
+			//gravity
+			NetForceTangant -= Math.sin(blob.orientation);
+			NetForceNormal -= Math.cos(blob.orientation);
+			//tension (related to area for philic)
+			if(s.IsHydroPhilic)
+				NetForceNormal -= tension*blob.unstressedsize*blob.aspectratio;
+			//dampening (only if it is philic)
+			if(s.IsHydroPhilic)
+			{
+				double TangentalVelocity = blob.VelocityX
+				NetForceTangant += (tangental velocity)
+			}
+						//DI
 			
+			//restoring and aspect ratio, springiness
 			
+			//normal (not sure how to implement)
 		}
 		else
 		{
+			blob.aspectratio=1;
 			NetForceDown += gravity;
-			if()
-			NetForceRight += 0.2*directionalinfluince*((int)RightPressed-(int)LeftPressed);
+			if(RightPressed)
+			{
+				NetForceRight += 0.2*directionalinfluince;
+			}
+			if(LeftPressed)
+			{
+				NetForceRight += -0.2*directionalinfluince;
+			}
 		}
+		//handle changing the accelerations
+		//handle moving
 	}
 	
 	private boolean colliding()//checks if the blob is close to a surface, then sets the angle and flags collision for step
 	{
-		
 		return false;
+	}
+	
+	private Surface SurfaceColiding()
+	{
+		
 	}
 	
 	public boolean haswon()
