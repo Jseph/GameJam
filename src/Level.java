2dbf09e16f6923c73a8a8f3cae841c131d2decd7
josep_000 <josep_000@JsephSurface>
2014-11-07 23:00:08 -0600

    Level and surface are updated

--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,37 @@
+import java.util.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.io.*;
 
-public class Level {
-
+public class Level 
+{
+	ArrayList<Surface> surfaces;
+	Point2D startPoint;
+	Rectangle2D endBox;
+	public Level(int number)
+	{
+		String levelPath = "level"+number+".dat";
+		surfaces = new ArrayList<Surface>();
+		try
+		{
+			Scanner scan = new Scanner(new File(levelPath));
+			double startX = scan.nextDouble();
+			double startY = scan.nextDouble();
+			startPoint = new Point2D.Double(startX,startY);
+			double endX1 = scan.nextDouble();
+			double endY1 = scan.nextDouble();
+			double endX2 = scan.nextDouble();
+			double endY2 = scan.nextDouble();
+			endBox = new Rectangle2D.Double(endX1, endY1, endX2, endY2);
+			int numSurfaces = scan.nextInt();
+			for(int i=0; i<numSurfaces; i++)
+			{
+				double sx = scan.nextDouble();
+				double sy = scan.nextDouble();
+				double ex = scan.nextDouble();
+				double ey = scan.nextDouble();
+				surfaces.add(new Surface(new Point2D.Double(sx, sy), new Point2D.Double(ex, ey), scan.nextBoolean()));
+			}
+		}catch (Exception e){System.out.println("Something went wrong reading input file "+levelPath+ "\n" + e);}
+	}
 }
--- src/Surface.java
+++ src/Surface.java
@@ -1,4 +1,14 @@
-
-public class Surface {
-
+import java.awt.geom.Point2D;
+import java.util.*;
+public class Surface 
+{
+	Point2D start;
+	Point2D end;
+	boolean hydrophobic;
+	public Surface(Point2D start, Point2D end, boolean hydrophobic)
+	{
+		this.start = start;
+		this.end = end;
+		this.hydrophobic = hydrophobic;
+	}
 }
